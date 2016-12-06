1cc16794493f6265cd75d46a9b689b1d92ac3d20
Alun Evans <alun@uber.com>
2016-12-05 21:05:27 -0800

    Fixes from new MBPr at uber.

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;; Save the starting time...
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defvar *init-start-time* (current-time))
 
 (setq load-path
@@ -66,7 +73,7 @@
 (require 'emoji-cheat-sheet-plus)
 (require 'gh)
 (require 'gist)
-(require 'git-modes)
+;(require 'git-modes)
 
 ;; End From HomeBrew.
 
